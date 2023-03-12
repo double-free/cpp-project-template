72f91b252666abd8be2259274a8836810839ad93
Ye Yuan <yuanye_ptr@qq.com>
2023-03-12 17:07:45 +0800

    naive unit test passed

--- src/volume_allocation/VolumeAllocation.cpp
+++ src/volume_allocation/VolumeAllocation.cpp
@@ -1,7 +1,9 @@
 #include "VolumeAllocation.hpp"
 
-#include <limits>
+#include <cassert>
 #include <iostream>
+#include <algorithm>
+#include <numeric>
 
 namespace algo {
   std::vector<Price> get_price_candidates(Price best_bid_price, Price best_ask_price, size_t max_levels)
@@ -42,6 +44,73 @@ namespace algo {
     return prices;
   }
 
+
+  // solve the unbounded knapsack problem
+  // extra contraint is that the knapsack must be full
+  [[nodiscard]] std::vector<TradeAllocation> solve_unbounded_knapsack(
+    Price total_amount, Quantity total_qty,
+    const std::vector<Price> prices)
+  {
+    using Solution = std::vector<Quantity>;
+    using Solutions = std::vector<std::vector<Quantity>>;
+    // we have to use 2D dp so that we can print the items
+    // dp[i][j] is the solution of selected items, -1 means no solution.
+    // For the first i items, to achieve total value j.
+    std::vector<std::vector<Solutions>> dp(prices.size() + 1);
+
+    for (auto&row : dp) {
+      // initialized with -1, invalid value
+      row.resize(total_amount + 1);
+      // the first value is valid, because a knapsack with 0 capacity is full
+      row[0] = {Solution(prices.size(), 0)};
+    }
+
+    std::vector<TradeAllocation> result;
+    for (int i = 1; i<dp.size(); ++i) {
+      const auto& px = prices.at(i-1);
+      for (Price j=px; j<dp[i].size(); ++j) {
+        // at least, with the extra ith item, it can have the same solution as
+        // the first i-1 items.
+        dp[i][j] = dp[i-1][j];
+        // the ith item can add another solution
+        for (const auto& prev_solution: dp[i][j-px]) {
+          // deep copy
+          auto solution = prev_solution;
+          // add one ith item
+          solution[i-1] += 1;
+          dp[i][j].push_back(solution);
+        }
+      }
+
+      for (const auto& solution: dp[i][total_amount])
+      {
+        if (std::accumulate(solution.begin(), solution.end(), 0) != total_qty) {
+          continue;
+        }
+
+        for (size_t i=0; i<solution.size(); ++i) {
+          const auto& qty = solution[i];
+          if (qty > 0) {
+            TradeAllocation allocation;
+            allocation.quantity = qty;
+            allocation.price = prices.at(i);
+            result.push_back(allocation);
+          }
+        }
+        // return on finding first solution
+        if (result.size() > 0) {
+          std::cout << "Find solution with total amount " << total_amount
+          << ", total quantity "<< total_qty << " with first " << i
+          << " items,TODO: output solution.\n";
+          return result;
+        }
+      }
+    }
+
+    // solution not found, probably choose a greater price range?
+    return result;
+  }
+
   std::vector<TradeAllocation> allocate_volume(
     Price total_amount, Quantity total_qty,
     Price best_bid_price, Price best_ask_price, size_t max_levels)
@@ -60,38 +129,33 @@ namespace algo {
     if (total_qty == 1) {
       TradeAllocation allocation;
       allocation.quantity = total_qty;
-      allocation.price_in_ticks = total_amount;
+      allocation.price = total_amount;
       return {allocation};
     }
 
     // multiple trades, we need serious allocation now
-    std::vector<TradeAllocation> result;
 
     // TODO: avoid dynamic memory allocation for price candidates
     // it is essentially an unbounded knapsack problem.
     // but we need to sort the price levels as we desire the trade to happen
     auto prices = get_price_candidates(best_bid_price, best_ask_price, max_levels);
 
-    // we have to use 2D dp so that we can print the items
-    std::vector<std::vector<Price>> dp(prices.size() + 1);
-    for (auto&row : dp) {
-      // initialized with -inf, invalid value
-      row.resize(total_qty + 1, std::numeric_limits<Price>::min());
-      // the first value is valid, because a knapsack with 0 capacity is full
-      row[0] = 0;
+    // we need to use the price offset instead of real price to save space
+    const auto& pivot_price = *std::min_element(prices.begin(), prices.end());
+    auto offset_prices = prices;
+    for (auto& px : offset_prices) {
+      px -= pivot_price;
     }
-    for (int i = 1; i<dp.size(); ++i) {
-      const auto& price = prices.at(i-1);
-      for (Quantity j=1; j<dp[i].size(); ++j) {
-        dp[i][j] = std::max(dp[i-1][j], dp[i-1][j-1] + price);
-        if (dp[i][j] == total_amount) {
-          std::cout << "Find solution with total amount " << total_amount
-          << ", total quantity "<< j << ", TODO: output solution.\n";
-          return result;
-        }
-      }
-    }
+    auto offset_total_amount = total_amount - pivot_price * total_qty;
+    assert(offset_total_amount > 0);
 
+    std::vector<TradeAllocation> result = solve_unbounded_knapsack(
+      offset_total_amount, total_qty, offset_prices);
+
+    for (auto& allocation : result) {
+      // restore offset price to original price
+      allocation.price += pivot_price;
+    }
     // solution not found!!!
     // probably you shall choose a greater "max_levels"
     return result;
--- src/volume_allocation/VolumeAllocation.hpp
+++ src/volume_allocation/VolumeAllocation.hpp
@@ -4,13 +4,13 @@
 #include <cstddef>
 
 namespace algo {
-  struct TradeAllocation {
-    int price_in_ticks{0};
-    int quantity{0};
-  };
-
   using Price = int;
   using Quantity = int;
+
+  struct TradeAllocation {
+    Price price{0};
+    Quantity quantity{0};
+  };
 
   std::vector<Price> get_price_candidates(Price best_bid_price, Price best_ask_price, size_t max_levels);
 
